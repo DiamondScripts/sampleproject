cffc55f4f8ad297ee3762d8e41c84dd5bc40b4ce
Sheyi Akingbade <sheyiakingbade@Sheyis-MacBook-Pro.local>
2018-01-29 14:50:55 +0100

    This is my first commit

---
+++ sample.py
@@ -0,0 +1,6 @@
+first = "this is me"
+second = "my name is sheyi akingbade"
+
+sentence = first + second
+
+print(sentence)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sheyiakingbade/PycharmProjects/sampleproject2/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
